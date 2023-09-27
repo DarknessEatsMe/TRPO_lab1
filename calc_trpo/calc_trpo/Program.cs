6476506a5838153ad03bbbc6cfac5f434e3559db
DarknessEatsMe <DarknessDevoursMe@yandex.ru>
2023-09-27 15:01:51 +0300

    Created a calculator

--- calc_trpo/calc_trpo/Program.cs
+++ calc_trpo/calc_trpo/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace calc_trpo
+﻿using System;
+
+namespace calc_trpo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double a;
+            double b;
+            double res;
+            char operation;
+
+            Console.WriteLine("Введите 1-е число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 2-е число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите операцию: ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            switch(operation)
+            {
+                case '+':
+                    res = a + b;
+                    Console.WriteLine("Сумма = " + res);
+                    break;
+
+                case '-':
+                    res = a - b;
+                    Console.WriteLine("Разница = " + res);
+                    break;
+
+                case '*':
+                    res = a * b;
+                    Console.WriteLine("Произведение = " + res);
+                    break;
+                case '/':
+                    res = a / b;
+                    Console.WriteLine("Частное = " + res);
+                    break;
+            }
         }
     }
 }
